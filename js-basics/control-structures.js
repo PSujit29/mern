7602eb8f65a7191d3d8dc4a222f4a368faf5d48a
PSujeet29 <sujeetpandey29061@gmail.com>
2025-08-08 16:00:16 +0545

    if, elseif and switch case

--- js-basics/control-structures.js
+++ js-basics/control-structures.js
@@ -1,5 +1,3 @@
-
-
 const day = "saturday"
 
 
@@ -45,3 +43,35 @@ switch (day) {
         console.log("invalid");
         break;
 }
+
+
+// Task:
+// 1. Create a JavaScript variable named unitConsumed and assign it a value greater than 0.
+// 2. Calculate the total electricity bill based on the following slab rates:
+//
+//    - For the first 20 units: fixed charge of 80
+//    - For the next 20 units (21-40): 5 per unit
+//    - For the next 30 units (41-70): 7.5 per unit
+//    - For the next 50 units (71-120): 12 per unit
+//    - For the next 100 units (121-220): 20 per unit
+//    - For units above 220: 25 per unit
+//
+// Write code to compute and display the bill amount for the given unitConsumed.
+
+const unitConsumed = 320
+let price = 0 // handle the base case
+if (unitConsumed <= 20) {
+    price = 80
+} else if (unitConsumed <= 40) {
+    price = 80 + (unitConsumed - 20) * 5
+} else if (unitConsumed <= 70) {
+    price = 80 + 20 * 5 + (unitConsumed - 40) * 7.5
+} else if (unitConsumed <= 120) {
+    price = 80 + 20 * 5 + 30 * 7.5 + (unitConsumed - 70) * 12
+} else if (unitConsumed <= 220) {
+    price = 80 + 20 * 5 + 30 * 7.5 + 50 * 12 + (unitConsumed - 120) * 20
+} else {
+    price = 80 + 20 * 5 + 30 * 7.5 + 50 * 12 + 100 * 20 + (unitConsumed - 220) * 25
+}
+
+console.log("price = ", price);
