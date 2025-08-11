eb8d535be281462d82bbe4a24c09c683e022cbb6
PSujeet29 <sujeetpandey29061@gmail.com>
2025-08-11 15:49:58 +0545

    for of and for in loops in array

---
+++ js-basics/menu-loop.js
@@ -0,0 +1,23 @@
+const categories =[
+    {
+        id:1,
+        name:"parts and machinary",
+    },
+    {
+        id:2,
+        name:"motorcycle",
+    },
+    {
+        id:3,
+        name:"pulsar 220",
+    }
+]
+
+for(const el of categories){
+    console.log(`${el.id}. ${el.name}`)
+}
+
+for(const i in categories){
+    let el = categories[i]
+    console.log(`${el.id}. ${el.name}`)
+}
