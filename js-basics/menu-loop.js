de9a9689d0ea94b01c7cf7609fef75ff602d017d
PSujeet29 <sujeetpandey29061@gmail.com>
2025-08-11 16:16:42 +0545

    for of loop and for in loops in arays

--- js-basics/menu-loop.js
+++ js-basics/menu-loop.js
@@ -1,23 +1,46 @@
-const categories =[
+const categories = [
     {
-        id:1,
-        name:"veichles",
+        id: 1,
+        name: "Electronics",
+        children: [
+            {
+                id: 2,
+                name: "Mobile Phones",
+                children: null
+            },
+            {
+                id: 3,
+                name: "Laptops",
+                children: null
+            }
+        ]
     },
     {
-        id:2,
-        name:"motorcycle",
-    },
-    {
-        id:3,
-        name:"pulsar 220",
+        id: 4,
+        name: "Clothing",
+        children: [
+            {
+                id: 5,
+                name: "Men",
+                children: null
+            },
+            {
+                id: 6,
+                name: "Women",
+                children: null
+            }
+        ]
     }
-]
+];
 
-for(const el of categories){
-    console.log(`${el.id}. ${el.name}`)
+for (const el of categories) {
+    console.log(`${el.id}. ${el.name}`);
 }
 
-for(const i in categories){
-    let el = categories[i]
-    console.log(`${el.id}. ${el.name}`)
+for (const i in categories) {
+    let el = categories[i];
+    console.log(`${el.id}. ${el.name}`);
 }
+
+// TODO:
+// complete the undesired nested loop to print out the hierarchical vategories
