92d2f0c8ef07c49bd2b920a7d1558688c8cfc0b1
PSujeet29 <sujeetpandey29061@gmail.com>
2025-08-15 15:55:51 +0545

    function types

---
+++ js-basics/function-types.js
@@ -0,0 +1,24 @@
+// types of function in js
+
+// 1. function with no argument and no return
+// 2. function with no argument and  return
+// 3. function with  argument and no return
+// 4. function with  argument and  return
+
+// defn types
+// declaration and arrow functions
+
+// iife
+// (()=>{
+//     console.log("hello, iife")
+// })();
+
+const userObject = {
+    name: "sujit pandey",
+    getName() { console.log(this.name) },
+    getAddress: function () { console.log(this.name) },
+    getEmail: () => { console.log(this.name) }, //do not use in object. scope of this variable is withinn the arrow block. 
+}
+userObject.getName()
+userObject.getAddress()
+userObject.getEmail()
