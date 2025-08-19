20944549dfc939387c154def3f884e1a7392388c
PSujeet29 <iamsujeet29@gmail.com>
2025-08-19 16:16:07 +0545

    promises chain

---
+++ js-basics/13-promise-chain.js
@@ -0,0 +1,74 @@
+const func1 = async (x) => {
+    if (x) return "I pass in func1"
+    else throw "I am reject from func 1"
+}
+
+const func2 = async (x) => {
+    if (x) return "I pass in func2"
+    else throw "I am reject from func 2"
+}
+
+const func3 = async (x) => {
+    if (x) return "I pass in func3"
+    else throw "I am reject from func 3"
+}
+
+const func4 = async (x) => {
+    if (x) return "I pass in func4"
+    else throw "I am reject from func 4"
+}
+
+// not the way. oh hell no 
+// func1(true)
+//     .then(res1 => {
+//         console.log(res1)
+
+//         func2(false)
+//             .then(res2 => {
+//                 console.log(res2)
+
+//                 func3(true)
+//                     .then(res3 => {
+//                         console.log(res3)
+
+//                         func4(false)
+//                             .then(res4 => {
+//                                 console.log(res4)
+//                             })
+//                             .catch(exception => {
+//                                 console.log(exception)
+//                             })
+//                     })
+//                     .catch(exception => {
+//                         console.log(exception)
+//                     })
+//             })
+//             .catch(exception => {
+//                 console.log(exception)
+//             })
+//     })
+//     .catch(exception => {
+//         console.log((exception))
+//     })
+
+
+
+func1(true)
+    .then(res1 => {
+        console.log(res1)
+        return func2(true)
+    })
+    .then(res2 => {
+        console.log(res2)
+        return func3(false)
+    })
+    .then(res3 => {
+        console.log(res3)
+        return func4(true)
+    })
+    .then(res4 => {
+        console.log(res4)
+    })
+    .catch(exception => {
+        console.log(exception)
+    })
